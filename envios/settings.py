e654b644f27fe62646276ccda3eb409fc5d610f1
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2025-11-06 20:49:53 -0500

    si esto no funciona pues pailin

--- envios/settings.py
+++ envios/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'envios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'provesidb'),
+        'USER': os.environ.get('DB_USER', 'abolivarc'),
+        'PASSWORD': os.environ.get('DB_PASS', 'Anjuboca200510'),
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': '5432',
     }
 }
 
